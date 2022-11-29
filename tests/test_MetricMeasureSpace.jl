01d4582aaf158da9b86cfb5c11178a6c80940eed
Willinki <d.badalotti@campus.unimib.it>
2022-11-29 16:45:47 +0100

    generated tests for MetricMeasureSpaces

--- tests/test_MetricMeasureSpace.jl
+++ tests/test_MetricMeasureSpace.jl
@@ -1,8 +1,33 @@
-import Test: @test, @test_throws
-import .ObjectMatching: MetricMeasureSpace
+import Test: @test, @test_throws, @testset
+import ObjectMatching: MetricMeasureSpace
 
-M = MetricMeasureSpace(ones(Float64, (2,2)), rand(Float64, 2))
 
-@test_throws ArgumentError MetricMeasureSpace(ones(Float64, (2,2)), Float64[-1, 1])
+@testset "MetricMeasureSpace" begin
+    dim::Int64 = rand(1:100)
+    C          = rand(Float64, (2,2))
+    μ          = rand(Float64, 2)
+    M          = MetricMeasureSpace(C, μ)
 
-@test sum(M.mu)==1
+    # negative entries in mu raise error 
+    @test_throws ArgumentError MetricMeasureSpace(C, Float64[-1, 1])
+
+    # once constructed, mu sums to 1
+    @test sum(M.μ)==1
+
+    function mu_is_constant_when_default(C)
+        mu_mms = MetricMeasureSpace(C).μ
+        return all(mu_mms .== mu_mms[1])
+    end
+    @test mu_is_constant_when_default(C)
+
+    # raise error when C and mu have different dimensions 
+    @test_throws ArgumentError MetricMeasureSpace(C, Float64[1, 1, 1])
+
+    # raise error when C is not square 
+    @test_throws ArgumentError MetricMeasureSpace(Float64[[1] [1]])
+    @test_throws ArgumentError MetricMeasureSpace(Float64[[1, 1, 1] [1, 1, 1]])
+
+    # raise error if \mu is always zero
+    @test_throws ArgumentError MetricMeasureSpace(C, zeros(Float64, dim))
+end 
+    
