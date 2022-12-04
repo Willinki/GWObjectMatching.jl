d02b56f6960c4b8ad27023b6633fb95dc3a68439
Willinki <d.badalotti@campus.unimib.it>
2022-12-04 19:15:44 +0100

    added tests

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -3,6 +3,7 @@ This script simply runs all the individual tests.
 """
 mytests = [
     "test_MetricMeasureSpace.jl"
+    "test_RepeatUntilConvergence.jl"
 ]
 println("Running tests...")
 for test in mytests
