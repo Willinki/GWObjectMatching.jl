467fdebe0aaf2a594f23967baacdd6314aaec0f6
Willinki <d.badalotti@campus.unimib.it>
2022-12-05 17:42:40 +0100

    rename Sinkhorn to SinkhornKnopp

--- src/SinkhornKnopp.jl
+++ src/SinkhornKnopp.jl
@@ -1,11 +1,11 @@
 import LinearAlgebra: diagm, norm
 
 """
-Data structure for sinkhorn (SH) iteration.
+Data structure for sinkhorn-knopp (SK) iteration.
 Has to be used inside RepeatUntilConvergence.
 # TODO: fill documentation
 """
-struct data_SH
+struct data_SK
     K::Matrix{Float64} 
     p::Vector{Float64} 
     q::Vector{Float64} 
@@ -17,7 +17,7 @@ end
 """
 Update for single iteration
 """
-function update_SH(elem::data_SH)::data_SH
+function update_SK(elem::data_SK)::data_SK
     elem.a .= (elem.p)./(((elem.K)')*(elem.b))
     elem.b .= (elem.q)./(((elem.K)')*(elem.a))
     elem.T .= diagm(elem.a)*elem.K*diagm(elem.b)
@@ -27,7 +27,7 @@ end
 """
 First proposal for stopping criterion, stops whenever transport is stable
 """
-function stop_SH_T(history::Vector{data_SH}; ϵ=10^{-8}::Float64)::Bool
+function stop_SK_T(history::Vector{data_SK}; ϵ=10^{-8}::Float64)::Bool
     length(history) == 1 && return false
     return norm(history[end].T - history[end-1].T,1) < ϵ
 end
@@ -36,10 +36,9 @@ end
 Second proposal for stopping criterion, stops whenever a and b are close
 enough to p and q.
 """
-function stop_SH_ab(history::Vector{data_SH}; ϵ=10^{-6}::Float64)::Bool
+function stop_SK_ab(history::Vector{data_SK}; ϵ=10^{-6}::Float64)::Bool
     return max(
         norm(history[end].a - history[end].p,1) ,
         norm(history[end].b - history[end].q,1)  
     ) < ϵ
-end 
-
+end
