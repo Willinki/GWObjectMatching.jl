24ecd62b029784909cd7fde3c4d6540c72c51a61
Willinki <d.badalotti@campus.unimib.it>
2022-12-21 11:11:21 +0100

    done docstrings

--- src/RepeatUntilConvergence.jl
+++ src/RepeatUntilConvergence.jl
@@ -1,31 +1,53 @@
 import DataStructures: CircularBuffer
 import Base
 
-"""This object aims at simulating a loop until convergence, we would like to get rid
-of while loops in the main algorithms.
 
-Arguments
----------
+"""This object aims at simulating a loop until convergence. In the loop, a
+a quantity is updated according to an update function. After updating, we check
+convergence with the has_converged function. The history allows to compare 
+values at different cylces of the iteration. Initial values can be supplied
+directly at execution (execute! function).
 
-- update_func: Function. It performs calculations and returns a value.
-- has_converged: Function. Takes history as input.
-Returns a boolean value, if True, the loop is stopped.
-- init_vals: Any. Initial values for update_func
-- history: contains values of returned parameters.
-# TODO: FIX DOCUMENTATION
-# TODO: INSERT THAT IF MEMORY SIZE IS ONE, history is not a vector 
-# TODO: maybe mutable can be removed
+Elements
+--------
+- update_func: Function. Must accept an element of type T as ONLY positional 
+    arguments and return the updated element of type T. Any other argument can 
+    be passed as keyword.
+- has_converged: Function. Takes Vector{T} as input (the history). 
+    Outputs true if the values have converged.
+- init_vals: T. Initial value for the loop
+- history: [specified in subclasses]. Keeps track of previous results 
+of the iterations. 
 """
 abstract type BaseRepeatUntilConvergence{T} end
 
+
+"""
+See BaseRepeatUntilConvergence.
+Implements RepeatUntilConvergence where history is supposed to be a short 
+(in the order of ~10) iterable of space-consuming quantities (for example
+large matrices). 
+
+In this instance history is a CircularBuffer. It keeps track of only the 
+last iterations (specified at construction) efficiently.
+
+Constructor Arguments
+---------------------
+- update_func: Function. Must accept an element of type T as ONLY positional 
+    arguments and return the updated element of type T. Any other argument can 
+    be passed as keyword.
+- has_converged: Function. Takes Vector{T} as input (the history). 
+    Outputs true if the values have converged.
+- memory_size: Int64 [default 2]. Maximum length of the history.
+"""
 mutable struct RepeatUntilConvergence{T} <: BaseRepeatUntilConvergence{T}
-    update_func::S where S<:Function
+    update_func::Function
     has_converged::Function
     history::CircularBuffer{T}
     init_vals::T
 
     function RepeatUntilConvergence{T}(
-        update_func::S where S<:Function,
+        update_func::Function,
         has_converged::Function;
         memory_size=2::Int64
         ) where T
@@ -46,6 +68,22 @@ mutable struct RepeatUntilConvergence{T} <: BaseRepeatUntilConvergence{T}
     end
 end
 
+
+"""
+See BaseRepeatUntilConvergence.
+Implements RepeatUntilConvergence where history is supposed to be a long 
+(in the order of ~100) iterable.
+
+In this instance history is a Vector. Keeps track of all the iterations.
+
+Constructor Arguments
+---------------------
+- update_func: Function. Must accept an element of type T as ONLY positional 
+    arguments and return the updated element of type T. Any other argument can 
+    be passed as keyword.
+- has_converged: Function. Takes Vector{T} as input (the history). 
+    Outputs true if the values have converged.
+"""
 mutable struct LargeMemoryRepeatUntilConvergence{T} <: BaseRepeatUntilConvergence{T}
     update_func::S where S<:Function
     has_converged::Function
@@ -55,12 +93,27 @@ mutable struct LargeMemoryRepeatUntilConvergence{T} <: BaseRepeatUntilConvergenc
     function LargeMemoryRepeatUntilConvergence{T}(
         update_func::Function,
         has_converged::Function;
-        memory_size=10::Int64
         ) where T
         throw("Not implemented")
     end
 end
 
+
+"""
+See BaseRepeatUntilConvergence.
+Implements RepeatUntilConvergence where history is supposed to be only the result 
+of the previous iteration .
+
+In this instance history is of type T. Keeps track of only the previous iterations.
+
+Constructor Arguments
+---------------------
+- update_func: Function. Must accept an element of type T as ONLY positional 
+    arguments and return the updated element of type T. Any other argument can 
+    be passed as keyword.
+- has_converged: Function. Takes ::T as input (the history). 
+    Outputs true if the values have converged.
+"""
 mutable struct SingleValueRepeatUntilConvergence{T} <: BaseRepeatUntilConvergence{T}
     update_func::Function
     has_converged::Function
@@ -70,7 +123,6 @@ mutable struct SingleValueRepeatUntilConvergence{T} <: BaseRepeatUntilConvergenc
     function SingleValueRepeatUntilConvergence{T}(
         update_func::Function,
         has_converged::Function;
-        memory_size=10::Int64
         ) where T
         throw("Not implemented")
     end
@@ -78,18 +130,66 @@ end
 
 
 """
-Initializes initial values. Performs a while loop with specified conditions.
-Returns the last results and R.
-# TODO: CircularBuffer is not optimal since convert takes O(n), might
-require an ad hoc thing in certain cases
+Executes loop until convergence given a set of initial values.
+
+Arguments
+---------
+- `R <: BaseRepeatUntilConvergence{T}`. Object that defines the loop.
+- `init_vals : T`. Initial values for the iterations.
+
+Returns
+-------
+`(iter_results, R) : Tuple{T, RepeatUntilConvergence{T}}`
+
+- `iter_results` are the results of the last iteration. 
+- `R` is the updated `RepeatUntilConvergence` object.
 """
-function execute!(R::RepeatUntilConvergence, init_vals)
-    R.init_vals = init_vals
+function execute!(R<:BaseRepeatUntilConvergence{T}, init_vals::T)
+    R.init_vals = deepcopy(init_vals)
     iter_results = R.update_func(R.init_vals)
-    push!(R.history, iter_results)
-    while !R.has_converged(convert(Vector, R.history))
+    update_history!(R, iter_results)
+    while has_converged_wrapper(R)
        iter_results = R.update_func(iter_results)
-       push!(R.history, iter_results) 
+       update_history!(R, iter_results)
     end
     return iter_results, R
+end
+
+"""
+Updates the history of a `BaseRepeatUntilConvergence{T}` object with 
+the last iteration results.
+
+## Returns nothing
+"""
+function update_history!(
+        R::Union{RepeatUntilConvergence{T}, LargeMemoryRepeatUntilConvergence{T}},
+        iter_results::T
+    )
+    push!(R.history, deepcopy(iter_results))
+end
+
+function update_history!(
+        R::SingleValueRepeatUntilConvergence{T},
+        iter_results::T
+    )
+    push!(R.history, deepcopy(iter_results))
+end
+
+"""
+Checks convergence of a `BaseRepeatUntilConvergence{T}` object based 
+on values in his `history`.
+
+Returns
+-------
+- `true` if R.has_converged returns `true`
+- `false` otherwise  
+"""
+function has_converged_wrapper(R::RepeatUntilConvergence{T})::Bool
+    return !R.has_converged(convert(Vector, R.history))
+end
+
+function has_converged_wrapper(
+        R::Union{LargeMemoryRepeatUntilConvergence{T}, SingleValueRepeatUntilConvergence{T}}
+    )::Bool 
+    return !R.has_converged(R.history)
 end
